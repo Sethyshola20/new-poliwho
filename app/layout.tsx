b7c1bf446df961da832fcb3b5d02a6c117261a4b
Sethyshola20 <sethylunelo@gmail.com>
2023-02-10 19:11:17 +0100

    styling done

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="fr">
       <Head />
       <body>{children}</body>
     </html>
