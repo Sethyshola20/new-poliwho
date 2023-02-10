23a49c8eb66013138c92d3ff22cfcbd906456b8f
Sethyshola20 <sethylunelo@gmail.com>
2023-02-10 17:07:12 +0100

    replicate

--- app/page.tsx
+++ app/page.tsx
@@ -1,91 +1,48 @@
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from './page.module.css'
+"use client";
 
-const inter = Inter({ subsets: ['latin'] })
+import React, { useState, useEffect, Suspense } from "react";
+import Representatives from "./components/representatives";
 
-export default function Home() {
+function App() {
+  const [representatives, setRepresentatives] = useState<any[]>([]);
+  const [search, setSearch] = useState("");
+  const [loading, setLoading] = useState(true);
+
+  const fetchRepresentative = async () => {
+    const res = await fetch("https://www.nosdeputes.fr/deputes/json");
+    const data = await res.json();
+
+    if (data.deputes) {
+      setRepresentatives(data.deputes);
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchRepresentative();
+  }, []);
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <>
+      <div className="form">
+        <form>
+          <input
+            className="search_input"
+            type="text"
+            placeholder="Nom, parti, departement..."
+            onChange={(e) => setSearch(e.target.value.toLowerCase())}
+          />
+        </form>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
-        </div>
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+      {loading ? (
+        <div>loading...</div>
+      ) : (
+        <h1>Les députés de la 15e législature:</h1>
+      )}
+      <Suspense fallback={<div>loading...</div>}>
+        <Representatives representatives={representatives} search={search} />
+      </Suspense>
+    </>
+  );
 }
+
+export default App;
